c52091bf9f7e06561bb49639d3ce986ca7d82006
jeantroiani <digitalguest@gmail.com>
2014-06-24 15:37:45 +0100

    new array added

--- directory.rb
+++ directory.rb
@@ -1,32 +1,63 @@
 student_count = 25
 #first we print the list if students
-puts "The students of my cohort at Makers Academy"
-puts "-------------"
-puts "Dave"
-puts "Eddie"
-puts "Catharina"
-puts "Igor"
-puts "Marco"
-puts "Lisa"
-puts "Michiel"
-puts "Jean"
-puts "Nicola"
-puts "Jennie"
-puts "Iona"
-puts "Nikesh"
-puts "Chloe"
-puts "Toan"
-puts "Jamie"
-puts "Peter"
-puts "Talal"
-puts "Charlie"
-puts "Charlotte"
-puts "Thomas"
-puts "Zoey"
-puts "Hannah"
-puts "Joe"
-puts "Alex"
-puts "Jeremy"
+students = [
+"Dave",
+"Eddie",
+"Catharina",
+"Igor",
+"Marco",
+"Lisa",
+"Michiel",
+"Jean",
+"Nicola",
+"Jennie",
+"Iona",
+"Nikesh",
+"Chloe",
+"Toan",
+"Jamie",
+"Peter",
+"Talal",
+"Charlie",
+"Charlotte",
+"Thomas",
+"Zoey",
+"Hannah",
+"Joe",
+"Alex",
+"Jeremy",
+]
+
+# and then print them
+ "The students of my cohort at Makers Academy"
+"-------------"
+puts students[0]
+puts students[1]
+puts students[2]
+puts students[3]
+puts students[4]
+puts students[5]
+puts students[6]
+puts students[7]
+puts students[8]
+puts students[9]
+puts students[10]
+puts students[11]
+puts students[12]
+puts students[13]
+puts students[14]
+puts students[15]
+puts students[16]
+puts students[17]
+puts students[18]
+puts students[19]
+puts students[20]
+puts students[21]
+puts students[22]
+puts students[23]
+puts students[24]
+puts students[25]
+
 #finally, we print the total
 puts "Overall we have #{student_count} great students"
 
