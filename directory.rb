5de6f52a4cc878fb7e8bbc19f5d71b3f33429dd8
jeantroiani <digitalguest@gmail.com>
2014-06-24 16:18:18 +0100

    changed array into hashes

--- directory.rb
+++ directory.rb
@@ -1,42 +1,51 @@
 
 #first we print the list if students
 students = [
-"Dave",
-"Eddie",
-"Catharina",
-"Igor",
-"Marco",
-"Lisa",
-"Michiel",
-"Jean",
-"Nicola",
-"Jennie",
-"Iona",
-"Nikesh",
-"Chloe",
-"Toan",
-"Jamie",
-"Peter",
-"Talal",
-"Charlie",
-"Charlotte",
-"Thomas",
-"Zoey",
-"Hannah",
-"Joe",
-"Alex",
-"Jeremy",
+{name: "Dave", cohort:  :June},
+{name: "Eddie", cohort: :June},
+{name: "Catharina" , cohort: :June},
+{name: "Igor" , cohort: :June},
+{name: "Marco", cohort: :June},
+{name: "Lisa", cohort: :June},
+{name: "Michiel", cohort: :June},
+{name: "Jean", cohort: :June},
+{name: "Nicola", cohort: :June},
+{name: "Jennie", cohort: :June},
+{name: "Iona", cohort: :June},
+{name: "Nikesh", cohort: :June},
+{name: "Chloe", cohort: :June},
+{name: "Toan", cohort: :June},
+{name: "Jamie", cohort: :June},
+{name: "Peter" , cohort: :June},
+{name: "Talal", cohort: :June},
+{name: "Charlie", cohort: :June},
+{name: "Charlotte", cohort: :June},
+{name: "Thomas", cohort: :June},
+{name: "Zoey", cohort: :June},
+{name: "Hannah", cohort: :June},
+{name: "Joe", cohort: :June},
+{name: "Alex", cohort: :June},
+{name: "Jeremy", cohort: :June}
 ]
 
+def print_header
 # and then print them
- "The students of my cohort at Makers Academy"
-"-------------"
-students.each do |names|
-	puts names
-	end 
+ puts "The students of my cohort at Makers Academy"
+puts "-------------"
+end
+
+def print(names)
+	names.each do |name|
+		puts "#{name[:name]} , (#{name[:cohort]} cohort)"
+	end
+end
+
+def print_footer(names)
+	puts "Overall we have #{names.length} great students"
+end
 
 
-
-#finally, we print the total
-puts "Overall we have #{students.length} great students"
+print_header
+print(students)
+print_footer(students)
 
