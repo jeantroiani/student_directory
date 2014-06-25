1e38efb7b3484e2e90eca53a98aaeffb33760f10
jeantroiani <digitalguest@gmail.com>
2014-06-25 11:54:40 +0100

    This time we added more questions for the user

--- directory.rb
+++ directory.rb
@@ -10,7 +10,7 @@ end
 def my_print(students)
 	 counter = 0
 	 while counter < students.count
-	 puts  "#{counter+1} #{students[counter][:name]}  #{students[counter][:cohort]} cohort"
+	 puts  "#{counter+1} #{students[counter][:name]} | #{students[counter][:cohort]} cohort | #{students[counter][:hobby]} | #{students[counter][:country]} | #{students[counter][:height]}"
 	 counter += 1
 	end
 end
@@ -24,12 +24,29 @@ end
 def input_students
 	puts "Please enter the names of the students"
 	puts"To finish, just hit return twice"
+	
 	students = []
+	
 	name = gets.chomp
+	puts "Please enter the hobby of the students"
+	hobby = gets.chomp
+	puts "Please enter the country of the students"
+	country= gets.chomp
+	puts "Please enter the height of the students"
+	height = gets.chomp
 	while !name.empty? do
-		students << {name: name , cohort: :June}
+		students << {name: name , cohort: :June, hobby: hobby, country: country, height: height }
 		puts "Now we have #{students.length} students"
+		puts "Please enter the names of the students"
 		name = gets.chomp
+		if !name.empty?
+			puts "Please enter the hobby of the students"
+			hobby = gets.chomp
+			puts "Please enter the country of the students"
+			country= gets.chomp
+			puts "Please enter the height of the students"
+			height = gets.chomp
+		end
 	end
 	 students
 
